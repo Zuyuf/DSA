db9c265d80dd8f386d552e01df546f92041a5442
Zuyufullah Manna <zuyufmanna@gmail.com>
2022-10-12 10:19:20 +0530

    patterns: inverse pyramid problem statement

---
+++ patterns/8_inverse_pyramid_pattern.ts
@@ -0,0 +1,24 @@
+// 8_inverse_pyramid_pattern.ts
+
+/**
+ * 
+ * Input: 5
+
+Output:
+
+*********
+ *******
+  *****
+   ***
+    *
+ * 
+ */
+
+
+
+
+
+
+
+export {}; // 👈️ make file ES Module
+// otherwise causes the Duplicate function implementation error glitch due to legacy script files
