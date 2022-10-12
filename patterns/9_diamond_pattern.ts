9cd2a148af67d2805ad504528914a7cead156af4
Zuyufullah Manna <zuyufmanna@gmail.com>
2022-10-12 10:21:29 +0530

    patterns: diamond problem statement

---
+++ patterns/9_diamond_pattern.ts
@@ -0,0 +1,28 @@
+// 9_diamond_pattern copy.ts
+
+/**
+ * 
+ * Input: 5
+
+Output:
+
+    *
+   ***  
+  *****
+ *******
+*********
+*********
+ *******
+  *****
+   ***
+    *
+    
+ * 
+ */
+
+
+
+
+
+export {}; // 👈️ make file ES Module
+// otherwise causes the Duplicate function implementation error glitch due to legacy script files
