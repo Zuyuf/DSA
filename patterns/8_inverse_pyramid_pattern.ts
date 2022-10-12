fc5a0298fe725c4a653d0b9a9f222d8da3d99219
Zuyufullah Manna <zuyufmanna@gmail.com>
2022-10-12 10:19:57 +0530

    patterns: inverse pyramid solution

--- patterns/8_inverse_pyramid_pattern.ts
+++ patterns/8_inverse_pyramid_pattern.ts
@@ -15,8 +15,20 @@ Output:
  */
 
 
+   function printInversePyramidPattern(input: number, char = '*') {
+    let line = '';
 
+    for (let i = input; i >= 1; i--) {
+        line = '';
 
+        if (i !== input) line = ' '.repeat(input - i);
+        line += char.repeat(2 * i - 1);
+        
+        console.log(line);
+    }
+}
+
+printInversePyramidPattern(5);
 
 
 
